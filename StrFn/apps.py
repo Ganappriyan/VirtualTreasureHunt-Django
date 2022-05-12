a113cfcffae7d1d8c36ea4ba0920a50e169d417a
Ganappriyan <ganappriyanc@gmail.com>
2022-05-13 05:18:01 +0530

    Created Django Structure, Temp Web App

---
+++ StrFn/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class StrfnConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'StrFn'
---
+++ TryMultipleTimes/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+count = 0
+
+def index(request): # Render Page & Validation
+  if(request.method == 'POST'):
+    done = request.POST.get('done')
+    if(done=='true'):
+      return render(request, 'TryMultipleTimes.html') # TODO: Go to Next Page
+    global count
+    print('\nCount: ', count) # TODO: Remove tmp Line
+    count+=1
+    if(count > 10):
+      return render(request, 'TryMultipleTimesComplete.html')
+    if(count > 1):
+      return render(request, 'TryMultipleTimes.html', {'res':'Wrong Password, TRY AGAIN!'})
+  
+  return render(request, 'TryMultipleTimes.html')
