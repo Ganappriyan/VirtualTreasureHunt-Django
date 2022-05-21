69b2c38d52f2a8caebf4f90bd010f005863197aa
Ganappriyan <ganappriyanc@gmail.com>
2022-05-21 20:08:49 +0530

    Level 3 Complete

--- TryDifferentView/views.py
+++ TryDifferentView/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render
+from random import choice
+from django.shortcuts import redirect, render
+password = choice(['kgf', 'valimai', 'beast'])
 
 def index(request):
-  pass
+  global password
+  if(request.method == 'POST'):
+    if(request.POST.get('password') == password):
+      return redirect('/f1insp')
+    return render(request, 'TryDifferentView.html', {'pass': password, 'cmd': 'Wrong Password'})
+  
+  return render(request, 'TryDifferentView.html', {'pass': password})
