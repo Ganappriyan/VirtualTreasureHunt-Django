3a3403a4d6dda2cac193aa5a3eabc11a6b45e422
Ganappriyan <ganappriyanc@gmail.com>
2022-05-22 11:18:46 +0530

    Bugs Fixed

--- QnA/views.py
+++ QnA/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 
 def index(request):
-  return render('QnA.html')
+  ques = [['The average of 20 numbers is zero. Of them, at the most, how many may be greater than zero?',
+               'Engineering is a,',
+               'What is Full Form of "GOOGLE"?'],
+               ['Spot the odd one out - FIRST, SECOND, THIRD, FORTH, FIFTH, SIXTH, SEVENTH, EIGHTH',
+                '',
+                '']]
+  c = 0
+  return render('QnA.html', {'q1': ques[c][0], 'q2': ques[c][1], 'q3': ques[c][2]})
