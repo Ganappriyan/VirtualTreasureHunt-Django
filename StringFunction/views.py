5ddde0356bfc963654b907d765092c995ddcb809
Ganappriyan <ganappriyanc@gmail.com>
2022-05-22 16:32:20 +0530

    Fixed Bugs

--- StringFunction/views.py
+++ StringFunction/views.py
@@ -6,4 +6,4 @@ def index(request):
     if(request.method == 'POST'):
         return render(request, 'StringFunctionComplete.html')
     passwords = ['narocoo@@ggo', 'ldie@fldgor@lako', 'peuiv@gerevr@vene', 'atperep@eeslt@ea', 'annmro@iami@']
-    return render(request, 'StringFunction.html', {'pass': choice(passwords)})
+    return render(request, 'StringFunction.html', {'password': choice(passwords)})
--- TryDifferentView/views.py
+++ TryDifferentView/views.py
@@ -1,10 +1,20 @@
 from random import choice
+from django.http import response
 from django.shortcuts import redirect, render
 
 def index(request):
-  password = choice(['free fire', 'don don don', 'shawarma', 'addict', 'pubg'])
+  if('pass' in request.COOKIES):
+    print("Cookies: ", request.COOKIES['pass'])  #TODO: Remove
+  
   if(request.method == 'POST'):
-    if(request.POST.get('password') == password):
+    print("Pass: ", request.POST.get('password')) # TODO: Remove
+    password = request.POST.get('password')
+    if(password == request.COOKIES['pass']):
       return render(request, 'TryDifferentViewComplete.html')
     return render(request, 'TryDifferentView.html', {'pass': password, 'cmd': 'Wrong Password'})
-  return render(request, 'TryDifferentView.html', {'pass': password})
+  
+  password = choice(['free fire', 'don don don', 'shawarma', 'addict', 'pubg'])
+  print('New Password: ', password) # TODO: Remove
+  response = render(request, 'TryDifferentView.html', {'pass': password})
+  response.set_cookie(key='pass', value=password)
+  return response
