bb3aaad2c9844e17924be702ecb75bd516767ae1
Ganappriyan <ganappriyanc@gmail.com>
2022-05-23 06:41:58 +0530

    Added DB for Final Rounds

--- DB/models.py
+++ DB/models.py
@@ -4,16 +4,20 @@ class Participants(models.Model):
   teamname = models.CharField(max_length=50, default='NULL')
   collegename = models.CharField(max_length=50, default='NULL')
   starttime1 = models.CharField(max_length=10, default='0:0:0')
-  starttime2 = models.CharField(max_length=10, default='0:0:0')
   level1 = models.CharField(max_length=10, default='0:0:0')
   level2 = models.CharField(max_length=10, default='0:0:0')
   level3 = models.CharField(max_length=10, default='0:0:0')
-  level4 = models.CharField(max_length=10, default='0:0:0')
-  level5 = models.CharField(max_length=10, default='0:0:0')
   totaltime1 = models.CharField(max_length=10, default='0:0:0')
-  totaltime2 = models.CharField(max_length=10, default='0:0:0')
   points1 = models.IntegerField(default=-1)
-  points2 = models.IntegerField(default=-1)
   
   def __str__(self):
     return self.teamname
+  
+class Finalers(models.Model):
+  teamname = models.CharField(max_length=50, default='NULL')
+  collegename = models.CharField(max_length=50, default='NULL')
+  starttime2 = models.CharField(max_length=10, default='0:0:0')
+  level4 = models.CharField(max_length=10, default='0:0:0')
+  level5 = models.CharField(max_length=10, default='0:0:0')
+  totaltime2 = models.CharField(max_length=10, default='0:0:0')
+  points2 = models.IntegerField(default=-1)
--- Home3/views.py
+++ Home3/views.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from django.shortcuts import redirect, render
 
-from DB.models import Participants
+from DB.models import Finalers
 
 
 def index(request):
@@ -13,11 +13,11 @@ def index(request):
         collegename = request.POST.get('collegename')
         request.session['teamname'] = teamname
         
-        if(Participants.objects.filter(teamname=teamname).exists()):
+        if(Finalers.objects.filter(teamname=teamname).exists()):
             return render(request, 'home.html', {'cmd': 'Team Name Taken, Try Different Name'})
           
-        data = Participants.objects.create(
-            teamname=teamname, collegename=collegename, starttime1=stime)
+        data = Finalers.objects.create(
+            teamname=teamname, collegename=collegename, starttime2=stime)
         data.save()
         
         return redirect('/f1insp')
