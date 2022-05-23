b2034d3d54fe15d9b56d1b14b9ec3315a233caed
Ganappriyan <ganappriyanc@gmail.com>
2022-05-23 05:47:08 +0530

    Added DB Handle for Level 2, 1&2

--- TryMultipleTimes/views.py
+++ TryMultipleTimes/views.py
@@ -1,11 +1,22 @@
+from datetime import datetime
 from django.shortcuts import redirect, render
-from django.http import HttpResponse
+
+from DB.models import Participants
 
 
 def index(request): # Render Page & Validation
   if(request.method == 'POST'):
     done = request.POST.get('done')
     if(done=='true'):
+      teamname = request.session['teamname']
+      collegename = request.session['collegename']
+      
+      ctime = datetime.now().strftime("%H:%M:%S")
+      
+      data = Participants.objects.get(teamname=teamname)
+      data.level2 = ctime
+      data.save()
+      
       return redirect('/3diff') # TODO: Go to Next Page
     return render(request, 'TryMultipleTimesComplete.html')
   
